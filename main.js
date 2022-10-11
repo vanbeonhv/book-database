7cd431f4fef4cd1643be2c4c20a0c4917e4aeb13
Marc <nguyenvannhv26@gmail.com>
2022-10-11 07:12:28 +0700

    add custom route

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/books/:id": "/api/books?ISBN=:id",
+  })
+);
 
-server.use(middlewares);
 server.use("/api", router);
 server.listen(3001, () => {
   console.log("JSON Server is running");
