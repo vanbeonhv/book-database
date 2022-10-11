6cedb36daad8fe11f71acbfe840f1b5c2d142797
Marc <nguyenvannhv26@gmail.com>
2022-10-11 18:08:45 +0700

    correct

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/books/:id": "/api/books?ISBN=:id",
-  })
-);
 
 server.use("/api", router);
 server.listen(3001, () => {
